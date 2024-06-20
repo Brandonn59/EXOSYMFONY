62afa416dfd504bed97dc860fb920d1d14f21697
Brandon <flamme.b@hotmail.com>
2024-06-20 16:23:34 +0200

    m

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,27 +3,55 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
-use Faker\Generator; 
-use App\Entity\Ingredient; 
+use Faker\Generator;
+use App\Entity\Ingredient;
+use App\Entity\Recipe; // Ensure this is the correct namespace
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
 
-    private Generator $faker; 
-    public function __construct(){
-        $this->faker = Factory::create('fr_FR'); 
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
     }
 
     public function load(ObjectManager $manager): void
     {
-        for ($i=0; $i<50; $i++) {
+        // Initialize ingredients array
+        $ingredients = [];
+        for ($i = 0; $i < 50; $i++) {
             $ingredient = new Ingredient();
-            $ingredient ->setName($this->faker->word())
-                        ->setPrice(mt_rand(0,100));
-        $manager ->persist($ingredient);
-    }
+            $ingredient->setName($this->faker->word())
+                       ->setPrice(mt_rand(0, 100));
+            $manager->persist($ingredient);
+            $ingredients[] = $ingredient; // Add to ingredients array
+        }
+
+        // Create recipes and add ingredients
+        for ($j = 0; $j < 25; $j++) {
+            $recipe = new Recipe();
+            $recipe->setName($this->faker->word())
+                   ->setTime(mt_rand(1, 1440))
+                   ->setNbPeople(mt_rand(0, 1) == 1 ? mt_rand(1, 50) : null)
+                   ->setDifficulty(mt_rand(0, 1) == 1 ? mt_rand(1, 5) : null)
+                   ->setDescription($this->faker->text(300)) // Fixed typo in 'setDescription'
+                   ->setPrice(mt_rand(0, 1) == 1 ? mt_rand(1, 1000) : null)
+                   ->setIsFavorite(mt_rand(0, 1) == 1);
+
+            for ($k = 0; $k < mt_rand(5, 15); $k++) {
+                {
+                    $recipe->addIngredient($ingredients[mt_rand(0,count($ingredients)-1)]);
+                }
+            }
+
+            $manager->persist($recipe);
+        }
+
+        // Save all data to the database
         $manager->flush();
     }
 }
+
